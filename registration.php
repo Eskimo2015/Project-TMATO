bae182d422ab9167762bfa6ca04b6e637e4775e5
Eskimo2015 <Noah_Nathan@192.168.1.9>
2016-09-15 10:20:09 +1200

    Updated Comments in PHP handler block - 'registration.php'.

--- registration.php
+++ registration.php
@@ -56,24 +56,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   		$pword = test_input($_POST["password"]);
   	}
   	if($fnameErr == "" && $lnameErr == "" && $dobErr == "" && $emailErr == "" && $unameErr == "" && $pwordErr == "") {
-  		//Step 2:  connect to MySQL and select database in one statement
+  		//Step 3:  connect to MySQL and select database in one statement
   		$connection = mysqli_connect("localhost:3306", "root", "", "tmato_db");
   	
-  		//Step 3:  Run query - check DB for existing account
+  		//Step 4:  Run query - check DB for existing account
   		$result = mysqli_query ($connection , "SELECT User_UName FROM user WHERE
   				User_UName LIKE '{$uname}';");
   	
-  		//Step 4a:  If username already exists then abort registration and print message
+  		//Step 5a:  If username already exists then abort registration and print message
   		if (mysqli_fetch_row($result)) {
   			header("Location: registration.php?reg_msg=Your chosen USERNAME is already in use!"
   					. "  Please enter another USERNAME to create a new account...");
-  			//Step 4b:  Run query - insert form data into user table and print confirmation message
+  			//Step 5b:  Run query - insert form data into user table and print confirmation message
   		} else {
   			mysqli_query($connection, "INSERT INTO user values(NULL,'{$fname}','{$lname}','{$dob}','{$email}',
   			'{$uname}','{$pword}','',CURDATE(),NULL,'0','0')");
   			header("Location: registration.php?reg_msg=Your account has been created successfully!");
   		}
-  		//Step 5:  Close connection
+  		//Step 6:  Close connection
   		mysqli_close($connection);
   	}
 }
