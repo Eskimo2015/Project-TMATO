8a4d18860e26e4ed6c214f22e8c3a6d1b6f50b1a
Eskimo2015 <Noah_Nathan@192.168.1.9>
2016-09-29 10:59:17 +1300

    Added $conn_err_msg to print db connection error in Registration and Login pages.  Added DB connection error messages in User page and Registration (User table).

--- login.php
+++ login.php
@@ -47,7 +47,10 @@ include 'handlers/login_handler.php';
                     <tr class="tr1" style="text-align:center"><td class="td1" colspan="3"><input class="btn" type="reset" name="reset" value="Reset" onclick="resetForm()"></td></tr>
                     <tr>
                         <td class="login_fail_msg" colspan="3">
-                            <?php echo $loginFail; ?>
+                            <?php 
+                            	echo $loginFail;
+								echo $conn_err_msg;
+							?>
                         </td>
                     </tr>
                 </table>
--- user.php
+++ user.php
@@ -74,41 +74,48 @@ Dislays user account details.
 					/*DB connect + output of needed fields
 					 * currently teams/orgs and bio are hard coded*/
                         include 'handlers/db_conn.php';
-                        $result = mysqli_query($connection, "SELECT * FROM user where User_UName LIKE '{$action}';");
                         
-                    
-                        echo"<h1>About</h1><div class='headingBreak'></div>";
-                        
-                        while ($output = mysqli_fetch_row($result)) {
-                            echo"
-								<p>
-									Name: $output[1] $output[2]
-								</p>
-								<p>
-									Date of birth: $output[7]
-								</p>
-								<p>
-									Email Address: $output[6]
-								</p>
-								<p>
-									Account Created: $output[8]
-								</p>
-								<p>
-									Bio: N/A
-								</p>
-								
-								<div class='spacerSmall'></div>
-								<h1>Teams</h1><div class='headingBreak'></div>
-								<p>
-									N/A
-								</p>
-								
-								<h1>Organisations</h1><div class='headingBreak'></div>
-								<p>
-									N/A
-								</p>
-                            ";
-                            mysqli_close($connection);
+                        if (!$connection) {
+                        	echo "<p class='conn_err_msg'>Unable to connect to database!  No data to display.<p>";
+                        	//$conn_err_msg = die('Connect Error: ' . mysqli_connect_error());
+                        } else {
+	                        $result = mysqli_query($connection, "SELECT * FROM user where User_UName LIKE '{$action}';");
+	                        
+	                    
+	                        echo"<h1>About</h1><div class='headingBreak'></div>";
+	                        
+	                        while ($output = mysqli_fetch_row($result)) {
+	                            echo"
+									<p>
+										Name: $output[1] $output[2]
+									</p>
+									<p>
+										Date of birth: $output[7]
+									</p>
+									<p>
+										Email Address: $output[6]
+									</p>
+									<p>
+										Account Created: $output[8]
+									</p>
+									<p>
+										Bio: N/A
+									</p>
+									
+									<div class='spacerSmall'></div>
+									<h1>Teams</h1><div class='headingBreak'></div>
+									<p>
+										N/A
+									</p>
+									
+									<h1>Organisations</h1><div class='headingBreak'></div>
+									<p>
+										N/A
+									</p>
+	                            ";
+	                        }
+	                        
+	                        mysqli_close($connection);
                         }
                     ?>
                 </table>
