0730b7d87611327f55848a6e06db1c02cb3b118a
Eskimo2015 <Noah_Nathan@192.168.1.9>
2016-10-20 13:43:21 +1300

    Fixed Team_Reg to registry associated Team table data accordingly. Fixed Team_Nav to display teams accordingly.

--- handlers/team_reg_handler.php
+++ handlers/team_reg_handler.php
@@ -12,8 +12,11 @@ $teamNameErr = $teamSportErr = "";
 $regSuccess = "";
 $conn_err_msg = "";
 
-$nameMatchExp = "/^[a-zA-Z0-9-_ ]*$/";
-$nameRangeExp = "/^[a-zA-Z0-9'- ]{0,32}$/";
+$teamNameMatchExp = "/^[a-zA-Z0-9-_ ]*$/";
+$teamNameRangeExp = "/^[a-zA-Z0-9-_ ]{0,32}$/";
+
+$teamSportMatchExp = "/^[a-zA-Z '-]*$/";
+$teamSportRangeExp = "/^[a-zA-Z '-]{0,32}$/";
 
 echo "gsjoizgsreiokh;gsrhnijogsr";
 
@@ -29,10 +32,10 @@ if (mysqli_connect_errno()) {
 	  	} else {
 	  		$teamName = clean_input($_POST['team_name']);
 	  		// check if Team Name only contains letters and whitespace
-	  		if (!preg_match($nameMatchExp,$teamName)) {
+	  		if (!preg_match($teamNameMatchExp,$teamName)) {
 	  			$teamNameErr = "Only upper and lower case letters (A-Z a-z), digits (0-9), hyphens (-), underscores (_) and Whitespace are permitted!";
 	  			// check if Team Name exceeds 32 characters
-	  		} else if(!preg_match($nameRangeExp,$teamName)) {
+	  		} else if(!preg_match($teamNameRangeExp,$teamName)) {
 	  			$teamNameErr = "Team Name must NOT exceed 32 characters!";
 	  		} else {
 	  			$teamNameErr = teamNameCheck($teamName);
@@ -43,10 +46,10 @@ if (mysqli_connect_errno()) {
 	  	} else {
 	  		$teamSport = clean_input($_POST['team_sport']);
 	  		// check if Team Sport only contains letters and whitespace
-	  		if (!preg_match($nameMatchExp,$teamSport)) {
+	  		if (!preg_match($teamSportMatchExp,$teamSport)) {
 	  			$teamSportErr = "Only letters, hyphens(-), apostrophes (') and white space are permitted!";
 	  			// check if Team Sport exceeds 32 characters
-	  		} else if(!preg_match($nameRangeExp,$teamSport)) {
+	  		} else if(!preg_match($teamSportRangeExp,$teamSport)) {
 	  			$teamSportErr = "Team Sport must NOT exceed 32 characters!";
 	  		}	
 	  	}
@@ -111,22 +114,22 @@ function insertTeamOwner($teamName){
 
 	//Links owner to team.
 	$teamID = getTeamID($teamName);
-	if (mysqli_query($connection, "
-			TRANSACTION 
-			INSERT INTO membership (Mem_State, Mem_Private, Mem_Description) 
-			values('{0}','{0}','{owner of a team}')
+	if (mysqli_query($connection, 
+			$memID = "INSERT INTO membership (Mem_State, Mem_Private, Mem_Description) 
+			values(0,0,'owner of a team')")) {
 			
-			$memID = SCOPE_IDENTITY();
-			
-			INSERT INTO t_member_of (User_ID, Team_ID, Role_ID, Mem_ID) 
-			values('{$_SESSION["uID"]}','{$teamID}','{1}','{$memID}')
-			
-			COMMIT
-			")) {
+		if (mysqli_query($connection, $memID)) {
+		    $last_id = mysqli_insert_id($connection);
+		    mysqli_query($connection, "INSERT INTO t_member_of (User_ID, Team_ID, Role_ID, Mem_ID) 
+			values('{$_SESSION['uID']}','{$teamID}',1,'{$last_id}')");
+		} else {
+		    $data = 'Error: ' . ' ' . $memID . ' ' . mysqli_error($connection);
+		}
 	}
 	else {
 		$data = "There was an issue creating your Team!  " . mysqli_error($connection);
-	}	
+	}
+	mysqli_close($connection);
 }
 
 function getTeamID($teamName){
@@ -139,6 +142,7 @@ function getTeamID($teamName){
 	while ($data = mysqli_fetch_row($result)){
 		return $data[0];
 	}
+	mysqli_close($connection);
 }
 
 ?>
--- team_navigation.php
+++ team_navigation.php
@@ -20,27 +20,37 @@
     	<div class="contentContainer">
         <div class="pageBreak"></div>
 	        <h1>
-	        	Current Teams 
+	        <?php
+				if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+					echo $_SESSION['user'] . '\'s Teams';
+				} else {
+					echo 'All Teams';
+				}
+			?> 
 	        </h1>
 	        <div class="headingBreak"></div>
         </div>
         <!-- Creating and displaying a list of all the teams -->
         <?php 
         	include "handlers/db_conn.php";
+        	$result;
         	
-        	$tName = mysqli_query($connection, "SELECT Team_Name FROM team;");
-        	$tSport = mysqli_query($connection, "SELECT Team_Sport FROM team;");
-        	
-        	$i=0;
-        	while (($sportOutput = mysqli_fetch_row($tSport)) && ($nameOutput = mysqli_fetch_row($tName))) {	
-        		$link = createURL($nameOutput[$i]);
-        		echo"
-        		<a href=$link><h1>$nameOutput[$i]</h1></a>
-        		<h3>$sportOutput[$i]</h3>
-        		<div class ='headingBreak'></div>"; 
+        	if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+				$result = mysqli_query($connection, "SELECT * FROM team WHERE Team_ID IN (SELECT Team_ID FROM t_member_of WHERE User_ID LIKE '{$_SESSION['uID']}');");
+        	} else {
+        		$result = mysqli_query($connection, "SELECT * FROM team;");
+        	}
 
-        	$i+1;
-			}
+        	while ($output = mysqli_fetch_row($result)) {
+        	
+        		$link = createURL($output[3]);
+        		echo"
+        		<a href=$link><h1>$output[3]</h1></a>
+        		<h3>$output[2]</h3><br/>
+        		<div class ='headingBreak'></div>";
+        	}
+        	
+        	mysqli_close($connection);
         ?>
         <p>
         	Want to create a new team? <a href = team_registration.php>Click here</a>
