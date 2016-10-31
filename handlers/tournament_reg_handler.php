7e43b5fa5d0aeadfe080a5be2204337c4a7aa6a5
Ben <ben.hook@hotmail.com>
2016-10-31 22:04:52 +1300

    Initial commits of tournament registration stuff

---
+++ handlers/tournament_reg_handler.php
@@ -0,0 +1,145 @@
+<?php
+//Created by: 	Noah Nathan
+//Date:			05/10/2016
+//Purpose:  	Validate Team Registration Form - fields.  Register New Team.
+
+include 'handlers/db_conn.php';
+
+//Step 1:  Define variables and set to empty values
+$teamName = $teamSport = "";
+$teamNameErr = $teamSportErr = "";
+
+$regSuccess = "";
+$conn_err_msg = "";
+
+$teamNameMatchExp = "/^[a-zA-Z0-9-_ ]*$/";
+$teamNameRangeExp = "/^[a-zA-Z0-9-_ ]{0,32}$/";
+
+$teamSportMatchExp = "/^[a-zA-Z '-]*$/";
+$teamSportRangeExp = "/^[a-zA-Z '-]{0,32}$/";
+
+//DB Connection Check!  If conection problems exist, print error on page.
+if (mysqli_connect_errno()) {
+	$conn_err_msg = "Unable to connect to database!  " . mysqli_connect_error();
+	//$conn_err_msg = die('Connect Error: ' . mysqli_connect_error());
+} else {
+	//Step 2:  If submission via POST method then validate...
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["team_name"])) {
+			$teamNameErr = "Team Name is required!";
+		} else {
+			$teamName = clean_input($_POST['team_name']);
+			// check if Team Name only contains letters and whitespace
+			if (!preg_match($teamNameMatchExp,$teamName)) {
+				$teamNameErr = "Only upper and lower case letters (A-Z a-z), digits (0-9), hyphens (-), underscores (_) and Whitespace are permitted!";
+				// check if Team Name exceeds 32 characters
+			} else if(!preg_match($teamNameRangeExp,$teamName)) {
+				$teamNameErr = "Team Name must NOT exceed 32 characters!";
+			} else {
+				$teamNameErr = teamNameCheck($teamName);
+			}
+		}
+		if (empty($_POST["team_sport"])) {
+			$teamSportErr = "Team Sport is required!";
+		} else {
+			$teamSport = clean_input($_POST['team_sport']);
+			// check if Team Sport only contains letters and whitespace
+			if (!preg_match($teamSportMatchExp,$teamSport)) {
+				$teamSportErr = "Only letters, hyphens(-), apostrophes (') and white space are permitted!";
+				// check if Team Sport exceeds 32 characters
+			} else if(!preg_match($teamSportRangeExp,$teamSport)) {
+				$teamSportErr = "Team Sport must NOT exceed 32 characters!";
+			}
+		}
+		if($teamNameErr == "" && $teamSportErr == "") {
+			$regSuccess = insertUserData($teamName, $teamSport);
+		}
+	}
+}
+
+//Trims and cleans input data/strings etc.
+function clean_input($data) {
+	$data = trim($data);
+	//$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+//Purpose:  Checks for availability of Team Name
+function teamNameCheck($teamName) {
+	$data = "";
+	//Step 1:  connect to MySQL and select database
+	include 'handlers/db_conn.php';
+	//Step 2:  Run query - check DB for existing Team
+	$result = mysqli_query ($connection , "SELECT Team_Name FROM team WHERE
+			Team_Name LIKE '{$teamName}';");
+	//Step 3:  If Team Name already exists then create error message
+	if (mysqli_fetch_row($result)) {
+		$data = "The selected Team Name is unavailable!  Please choose another TEAM NAME.";
+	} else {
+		$data = mysqli_error($connection);
+	}
+	//Step 4:  return error message
+	return $data;
+	//Step 5:  Close connection
+	mysqli_close($connection);
+}
+
+function insertUserData($teamName, $teamSport) {
+	$data = "";
+	//Step 1:  connect to MySQL and select database
+	include 'handlers/db_conn.php';
+
+	//Step 2:  Insert user data to User table and print confirmation message
+	if (mysqli_query($connection, "INSERT INTO team (Team_Name, Team_Sport) values('{$teamName}','{$teamSport}')")) {
+		resetFields();
+		insertTeamOwner($teamName);
+		$data = "Your Team has been created successfully!";
+	} else {
+		$data = "There was an issue creating your Team!  " . mysqli_error($connection);
+	}
+	return $data;
+
+	//Step 3:  Close connection
+	mysqli_close($connection);
+}
+function resetFields(){
+	$_POST["team_name"] = $_POST["team_sport"] = "";
+}
+
+function insertTeamOwner($teamName){
+	$memID = "";
+	include "db_conn.php";
+
+	//Links owner to team.
+	$teamID = getTeamID($teamName);
+
+	if (mysqli_query($connection,
+			$memID = "INSERT INTO membership (Mem_State, Mem_Private, Mem_Description)
+			values(0,0,'owner of a team')")) {
+				
+			if (mysqli_query($connection, $memID)) {
+				$last_id = mysqli_insert_id($connection);
+				mysqli_query($connection, "INSERT INTO t_member_of (User_ID, Team_ID, Role_ID, Mem_ID)
+						values('{$_SESSION['uID']}','{$teamID}',1,'{$last_id}')");
+			} else {
+				$data = 'Error: ' . ' ' . $memID . ' ' . mysqli_error($connection);
+			}
+
+			mysqli_close($connection);
+	}
+}
+function getTeamID($teamName){
+	include "db_conn.php";
+	if (!$connection) {
+		echo "<p class='conn_err_msg'>Unable to connect to database!  No data to display.<p>";
+	} else {
+		$result = mysqli_query($connection, "SELECT Team_ID FROM team where Team_Name LIKE '{$teamName}';");
+	}
+	while ($data = mysqli_fetch_row($result)){
+		return $data[0];
+	}
+	mysqli_close($connection);
+}
+
+?>
---
+++ tournament_registration.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<?php
+include 'session.php';
+include 'handlers/team_reg_handler.php';
+?>
+<html>
+    <head>
+        <title>Tournament Registration</title>
+        <link rel="stylesheet" type="text/css" href="css/tmato_theme.css"/>
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+        <meta charset="UTF-8">
+        <meta name="description" content="Team manager and tournament organiser">
+        <meta name="keywords" content="Tournament,Organiser,Tmato,Manager">
+        <meta name="author" content="Noah Nathan">
+        <meta name="author" content="Dion Rabone">
+        <!--Acknowledgements-->
+        <!--Graphical consultant: Giscarde Rousseau-->
+        <link rel="Background image provided by:" href="http://subtlepatterns.com/page/2/?s=light">
+        <link rel="Logo font provided by:" href="http://www.dafont.com/chavelite.font ">
+        <!--endOf-->
+        <script>
+			function resetForm() {
+			    document.getElementById("regForm").reset();
+			}
+		</script>
+    </head>
+    <body>
+        <!--Banner-->
+        <?php include 'banner.php'; ?>
+        <!--ContentBody-->
+        <div class="contentContainer">
+	        <div class="pageBreak"></div>
+	        <h1>
+	            Tournament Registration
+        	</h1>
+	        <div class="headingBreak"></div>
+	        <div class="setBodyMargin">
+	            <form id="regForm" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+	                <table class="tb1">
+	                    <tr><td class="td1">Team Name:</td><td></td><td><input class="input_reg_form" type="text" placeholder="Hurricanes" name="team_name" value="<?PHP if(isset($_POST['team_name'])) echo htmlspecialchars($_POST['team_name']); ?>"></td>
+	                    	<td><span class="error">* <?php echo $teamNameErr;?></span></td></tr>
+	                    <tr><td class="td1">Team Sport:</td><td></td><td><input class="input_reg_form" type="text" placeholder="Rugby" name="team_sport" value="<?PHP if(isset($_POST['team_sport'])) echo htmlspecialchars($_POST['team_sport']); ?>"></td>
+	                    	<td><span class="error">* <?php echo $teamSportErr;?></span></td></tr>
+	                    <tr><td class="td1" colspan="3"></td></tr>
+	                    <tr><td class="td1" colspan="3"></td></tr>
+	                    <tr class="tr1" style="text-align:center"><td class="td1" colspan="3"><input class="btn" type="submit" name="submit" value="Register Team"></td></tr>
+	                    <tr class="tr1" style="text-align:center"><td class="td1" colspan="3"><input class="btn" type="reset" name="reset" value="Reset" onclick="resetForm()"></td></tr>
+	                    <tr>
+	                        <td class="reg_success_msg" colspan="3">
+	                            <?php 
+	                            	echo $regSuccess;
+									echo $conn_err_msg;
+								?>
+	                        </td>
+	                    </tr>
+	                </table>
+	            </form>
+	        </div>
+	        <div>
+	            <br><br>
+	                    <?php
+	                    include 'handlers/db_conn.php';
+	                    if (!$connection) {
+							echo "<p class='conn_err_msg'>No data to display!</p>";
+	                    } else {
+				            echo '<div class="div_user">
+				                <table class="tbl_user">
+				                    <tr>
+				                        <th class="th_user">Team ID</th>   
+				                        <th class="th_user">Org ID</th>
+				                        <th class="th_user">Team Name</th>
+				                        <th class="th_user">Team Sport</th>
+				                        <th class="th_user">Bio</th>
+				                    </tr>';
+	                    
+							$result = mysqli_query($connection, "SELECT * FROM team;");
+							
+		                    while ($output = mysqli_fetch_row($result)) {
+		                        echo"
+		                                <tr>
+		                                    <td class='td_user'>$output[0]</td>
+		                                    <td class='td_user'>$output[1]</td>
+		                                    <td class='td_user'>$output[3]</td>
+		                                    <td class='td_user'>$output[2]</td>
+		                                    <td class='td_user'>$output[4]</td>
+		                                </tr>
+		                            ";
+		                    }
+		                    
+	                		mysqli_close($connection);
+	
+	                		echo "</table>
+	                		</div>";
+	                    }
+	                    ?>
+	        </div>
+	        <div>
+	            <br><br>
+	                    <?php
+	                    include 'handlers/db_conn.php';
+	                    if (!$connection) {
+							echo "<p class='conn_err_msg'>No data to display!</p>";
+	                    } else {
+				            echo '<div class="div_user">
+				                <table class="tbl_user">
+				                    <tr>
+				                        <th class="th_user">Team ID</th>   
+				                        <th class="th_user">User</th>
+				                    </tr>';
+	                    
+							$result = mysqli_query($connection, "SELECT * FROM t_member_of;");
+							
+		                    while ($output = mysqli_fetch_row($result)) {
+		                    	$user_result = mysqli_query($connection, "SELECT User_UName FROM user WHERE User_ID=".$output[0].";");
+		                    	$user_result = mysqli_fetch_row($user_result);
+		                        echo"
+		                                    <td class='td_user'>$output[1]</td>
+		                                    <td class='td_user'>$user_result[0]</td>
+		                                </tr>
+		                            ";
+		                    }
+		                    
+	                		mysqli_close($connection);
+	
+	                		echo "</table>
+	                		</div>";
+	                    }
+	                    ?>
+	        </div>
+        </div>
+    </body>
+</html>
