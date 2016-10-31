86240e7a2c06eac06fe6204cac3d771e5be59533
Ben <ben.hook@hotmail.com>
2016-10-31 19:25:22 +1300

    Added createTournamentUrl.

--- functions/create_url_function.php
+++ functions/create_url_function.php
@@ -1,17 +1,21 @@
-<?php 
+<?php
 /**
  * Included by 'team_navigation.php'.
  * @param unknown $team_name
  * @return string
  */
-	function createTeamURL($team_name){
-		$team_name = replaceWhiteSpace($team_name);
-		//$link = ("http://localhost/project_tmato/team.php" . "?action=" . $team_name);
-		$link = "team.php?action=" . $team_name;
-		return $link;
-	}
-	
-	function replaceWhiteSpace($string) {
-		return $string = preg_replace('/\s+/', '_', $string);
-	}
+function createTeamURL($team_name) {
+	$team_name = replaceWhiteSpace ( $team_name );
+	// $link = ("http://localhost/project_tmato/team.php" . "?action=" . $team_name);
+	$link = "team.php?action=" . $team_name;
+	return $link;
+}
+function createTournamentURL($tournament_name) {
+	$tournament_name = replaceWhiteSpace ( $tournament_name );
+	$link = "tournament.php?action=" . $tournament_name;
+	return $link;
+}
+function replaceWhiteSpace($string) {
+	return $string = preg_replace ( '/\s+/', '_', $string );
+}
 ?>
